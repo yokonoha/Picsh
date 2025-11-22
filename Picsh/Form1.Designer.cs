bd1a33f52fb779159b38a4eb422daf97f344a438
よこのは(横葉) <askjp.team@gmail.com>
2025-11-23 01:48:24 +0900

    first

---
+++ Picsh/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Picsh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1250, 604);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1250, 604);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "下をダブルクリックして写真を選択してください。 Picsh2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Picsh/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Picsh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            //すぐ開けるようにv1から仕様変更
+            openFile1.Filter = "画像データ|*.jpg;*.png;*.gif;*.bmp;*.tif";
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+            pictureBox1.Load(openFile1.FileName);
+            this.Text = "Picsh:"+openFile1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] fs = System.Environment.GetCommandLineArgs();
+            //複数エントリ
+            if (fs.Length > 1 && File.Exists(fs[1]))
+            {
+                    pictureBox1.ImageLocation = fs[1] ;
+
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //無視
+                return;
+            }
+                
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] fs)
+            {
+                pictureBox1.ImageLocation = fs[0];
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+
+                MessageBox.Show("ファイルのみドラッグアンドドロップが可能です。");
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button== MouseButtons.Right)
+            {
+                if (this.TopMost == true)
+                {
+                    this.TopMost = false;
+                    MessageBox.Show("-右クリックアクション-\n常に手前表示 を解除しました。");
+                }
+                else
+                {
+                    this.TopMost = true;
+                    MessageBox.Show("-右クリックアクション-\n常に手前表示 に設定しました。");
+                }
+            }
+
+            if(e.Button == MouseButtons.Middle)
+            {
+                MessageBox.Show("-ホイールクリックアクション-\n情報表示を行います。");
+                if (pictureBox1.Image == null)
+                {
+                    MessageBox.Show("画像が読み込まれていません。");
+                }
+                else
+                {
+                    MessageBox.Show("実際の画像サイズ:" + pictureBox1.Image.Size.ToString().Replace("Width=","幅(横サイズ)は").Replace("Height=","高さ(縦サイズ)は").Replace("{","").Replace(",", "px(ピクセル)、").Replace("}","px(ピクセル)です。") + "\n現在の表示枠サイズ:" + pictureBox1.Size.ToString().Replace("Width=", "幅(横サイズ)は").Replace("Height=", "高さ(縦サイズ)は").Replace("{", "").Replace(",", "px(ピクセル)、").Replace("}", "px(ピクセル)です。"));
+                    MessageBox.Show("画像ファイルサイズ:" + new FileInfo(pictureBox1.ImageLocation).Length.ToString("N0") + " バイトです。\n" + pictureBox1.ImageLocation+"\nに保存されている画像を表示しています。");
+                }
+                MessageBox.Show("Picshv2\n最軽量・必要最低限の画像表示ソフト\n 2018年7月に公開した初版にコマンドメニューを付け加え、更に内部処理も見直しました。\n 2025 横茶横葉 製作.");
+            }
+        }
+    }
+}
